deb0e95857c2a4f8b33af172440219a4bbb14a66
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-11-01 12:23:26 +0100

    Basic setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-export default class {
+import babel from 'broccoli-babel-transpiler';
 
-}
+export default {
+  order: 2,
+  exclude: ['**/*.js'],
+
+  build(tree, project) {
+    return babel(tree, {
+      babelrc: false,
+      compact: false,
+      plugins: [
+        ...project.addons
+        .map(a => a.babelOptions.plugins || [])
+        .reduce((all, list) => ([...all, ...list]), []),
+      ],
+      filterExtensions: [
+        'js',
+        ...project.addons
+        .map(a => a.babelOptions.filterExtensions || [])
+        .reduce((all, list) => ([...all, ...list]), []),
+      ],
+      presets: [
+        require.resolve('babel-preset-es2015'),
+        ...project.addons
+        .map(a => a.babelOptions.presets || [])
+        .reduce((all, list) => ([...all, ...list]), []),
+      ],
+      browserPolyfill: true,
+    });
+  },
+};
